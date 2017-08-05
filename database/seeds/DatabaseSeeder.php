236cffa2a23ad58cc14263f1fceba57384ed5ce2
laravelcoder <pmadsen2013@gamil.comgit>
2017-08-05 04:51:34 -0600

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        //$this->call(DesignsTableSeeder::class);
+        $this->call(DesignsTableSeeder::class);
         $this->call(PostsTableSeeder::class);
 
     }
